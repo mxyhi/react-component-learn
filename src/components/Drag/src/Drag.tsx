d49be2ed08cd3c359c87bb0066a8265d2cc65486
mxy <imxyi@hotmail.com>
2023-02-03 09:42:36 +0800

    init

--- src/components/Drag/src/Drag.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-export interface DragFC {}
-
-const Drag: FC<DragFC> = props => {
-  return <div>hello world</div>;
-};
-
-export default Drag;
--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{tsx,ts,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
